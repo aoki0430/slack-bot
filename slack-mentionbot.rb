f5adfc95e15b7b1333ed502af162a2f8839048b0
aoki0430 <31432520+aoki0430@users.noreply.github.com>
2019-12-03 13:50:55 +0900

    Update slack-mentionbot.rb

--- slack-mentionbot.rb
+++ slack-mentionbot.rb
@@ -15,45 +15,9 @@ require 'slack-ruby-client'
 
   # message eventを受け取った時の処理
   client.on :message do |data|
-    if data['attachments'][0]['text'].include?('@aoki0430')
-      client.message channel: data['channel'], text:'<@U8Z552LPL>'
+    if data['attachments'][0]['text'].include?('@githubname')
+      client.message channel: data['channel'], text:'<@SlackID>'
     end
-
-    if data['attachments'][0]['text'].include?('@chagara')
-      client.message channel: data['channel'], text:'<@chagara>'
-    end
-
-    # if data['attachments'][0]['text'].include?('@nakarin')
-    #   client.message channel: data['channel'], text:'<@nakarin>'
-    # end
-    #
-    # if data['attachments'][0]['text'].include?('@gaku2n')
-    #   client.message channel: data['channel'], text:'<@U8Z552LPL>'
-    # end
-    #
-    # if data['attachments'][0]['text'].include?('@mi2yay')
-    #   client.message channel: data['channel'], text:'<@U8Z552LPL>'
-    # end
-    #
-    # if data['attachments'][0]['text'].include?('@yoshixj')
-    #   client.message channel: data['channel'], text:'<@U8Z552LPL>'
-    # end
-    #
-    # if data['attachments'][0]['text'].include?('@29rou')
-    #   client.message channel: data['channel'], text:'<@U8Z552LPL>'
-    # end
-    #
-    # if data['attachments'][0]['text'].include?('@haliver')
-    #   client.message channel: data['channel'], text:'<@U8Z552LPL>'
-    # end
-    #
-    # if data['attachments'][0]['text'].include?('@ityc')
-    #   client.message channel: data['channel'], text:'<@U8Z552LPL>'
-    # end
-    #
-    # if data['attachments'][0]['text'].include?('@ryousuke.inoue')
-    #   client.message channel: data['channel'], text:'<@U8Z552LPL>'
-    # end
 
   end
 
